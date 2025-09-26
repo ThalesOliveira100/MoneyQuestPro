ebff4070ac63c1ccfb85d053341ac1004237ab67
Thales Oliveira <TOliveiraDev@outlook.com>
2025-09-25 22:44:58 -0300

    feat(cors): add cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import db from './config/dbConnect';
 import routes from './routes';
+import cors from 'cors';
+
+// Configuração do CORS
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', 'https://moneyquestpro.vercel.app'];
 
 db.on("error", console.log.bind("Erro durante a conexão com o banco de dados"));
 db.once("open", () => {
@@ -8,6 +12,18 @@ db.once("open", () => {
 });
 
 const app = express();
+app.use(cors({
+    origin: function (origin, callback) {
+        // Permitir requisições sem origem (como Postman)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'O CORS policy para este site não permite acesso a partir desta origem.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 app.use(express.json());
 app.use('/v1', routes);
 
